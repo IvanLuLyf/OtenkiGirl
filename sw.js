d1ea40257cc44d6e9ca39e6919674fefad6ae959
IvanLuLyf <835498692@qq.com>
2019-11-24 16:07:31 +0800

    Add ServiceWorker

---
+++ sw.js
@@ -0,0 +1,41 @@
+self.addEventListener('install', function (event) {
+    let languages = ['zh-cn', 'zh-tw', 'ja', 'en-us'];
+    let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
+    if (languages.indexOf(languageCode) === -1) languageCode = 'zh-cn';
+    event.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                '/OtenkiGirl/',
+                '/OtenkiGirl/index.html',
+                '/OtenkiGirl/favicon.ico',
+                '/OtenkiGirl/dialog.js',
+                '/OtenkiGirl/lang/' + languageCode + '.json',
+                '/OtenkiGirl/img/avatar.jpg',
+                '/OtenkiGirl/img/header.jpg',
+                '/OtenkiGirl/img/left.jpg',
+                '/OtenkiGirl/img/top.jpg',
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function (event) {
+    if (event.request.url.startsWith('https://kvdb.io/')) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+    event.respondWith(caches.match(event.request).then(function (response) {
+        if (response !== undefined) {
+            return response;
+        } else {
+            return fetch(event.request).then(function (response) {
+                let responseClone = response.clone();
+                caches.open('v1').then(function (cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            }).catch(function () {
+                return new Response('[]');
+            });
+        }
+    }));
+});
