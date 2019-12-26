8d4151c5508cc2a0e062e1e0514aa6a5f38f08f2
IvanLuLyf <835498692@qq.com>
2019-12-26 14:30:34 +0800

    update v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v7';
+const CATCH_VER = 'v8';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'zh-tw', 'ja', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
