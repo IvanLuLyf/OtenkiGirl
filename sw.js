c3f48d5bfa5a9e3d365c65b5d1828aa12d0ab185
IvanLuLyf <835498692@qq.com>
2020-02-14 13:13:55 +0800

    update v13 using Gun.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v12';
+const CATCH_VER = 'v13';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'zh-tw', 'ja', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
